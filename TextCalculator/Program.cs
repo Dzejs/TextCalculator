48dc33d7d06ee451075de7500690ee4b23ecc96e
Dzejs <boki-k4@live.com>
2022-03-13 19:53:35 +0100

    add: TextCalculator

---
+++ TextCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a string of numbers:");
+                string numbers = Console.ReadLine();
+
+                Console.WriteLine(TextCalculatorHelpers.Add(numbers));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+        }
+    }
+}
---
+++ TextCalculator/TextCalculatorHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace TextCalculator
+{
+    public static class TextCalculatorHelpers
+    {
+
+        public static string Add(string userNumbers)
+        {
+            if (userNumbers == "")
+                return "0";
+
+            if (userNumbers.Last() == ',')
+                throw new InvalidOperationException("Missing number in last position.");
+
+            
+            var negativeNumbers = userNumbers.Split(",")
+                                             .Where(x => double.TryParse(x, out double number) && number < 0)
+                                             .ToList();
+            if (negativeNumbers.Count > 0)
+                throw new Exception($"Negative not allowed {string.Join(',', negativeNumbers)}");
+
+            var result = userNumbers.Split(",")
+                         .Select(x =>
+                         {
+                             return double.TryParse(x, out double numberParsed) 
+                                ? numberParsed 
+                                : throw new Exception("There was a character in the string of numbers!");
+                         })
+                         .Sum();
+
+            return result.ToString();
+        }
+    }
+}
